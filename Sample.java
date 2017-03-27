3a06e32c60e4c8d0cad8ed9a8f3c0b04ba47fea1
gayuram <gayuramkumar25@gmail.com>
2017-03-27 15:37:52 +0530

    Create Sample.java

---
+++ Sample.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+
+class Sample
+{
+	public static void main (String[] args)
+	{
+  int n;
+		if(n>2)
+
+		  System.out.println("test");
+	}
+}
